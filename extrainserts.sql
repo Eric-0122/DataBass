42b725014bad2095982b5c2e186518f09e61f38e
SamMac55 <samanthamac055@gmail.com>
2026-04-04 23:26:10 -0500

    inserted so many new songs via csv and some hand done (also did accredations)-- updates the database export file

---
+++ extrainserts.sql
@@ -0,0 +1,7 @@
+INSERT INTO songs (song_id, album_id,song_title,duration) values (221,1,"Grenade", 3.70);
+INSERT INTO songs (song_id, album_id,song_title,duration) values (225,1,"Marry You", 3.83);
+INSERT INTO songs (song_id, album_id,song_title,duration) values (226,1,"Talking to the Moon", 3.62);
+INSERT INTO songs (song_id, album_id,song_title,duration) values (227,1,"Liquor Store Blues", 3.82);
+INSERT INTO songs (song_id, album_id,song_title,duration) values (228,1,"The Other Side", 3.78);
+
+INSERT INTO accredation values(666,221,1),(667,225,1),(668,226,1),(669,227,1),(670,228,1);
